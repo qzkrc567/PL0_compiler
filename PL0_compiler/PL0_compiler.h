c6c75438e0051eb66f852d6aa544b56fd476bf5f
qzkrc567 <424049382@qq.com>
2018-12-17 13:46:44 +0800

    添加项目文件。

---
+++ PL0_compiler/PL0_compiler.h
@@ -0,0 +1,32 @@
+﻿
+// PL0_compiler.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CPL0compilerApp:
+// 有关此类的实现，请参阅 PL0_compiler.cpp
+//
+
+class CPL0compilerApp : public CWinApp
+{
+public:
+	CPL0compilerApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CPL0compilerApp theApp;
